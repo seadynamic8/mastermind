a5ccd58c9cb34ff3865db5675103076f3fb2bd10
JarvisK <seadynamic8@zoho.com>
2015-07-17 12:24:03 -0400

    - Has rounds and roles - Computer has a simple AI - Scoring system is not working yet.

--- lib/mastermind/board.rb
+++ lib/mastermind/board.rb
@@ -1,13 +1,12 @@
 module Mastermind
 	class Board
-		attr_reader :code, :scores
-		attr_accessor :turns
+		attr_accessor :code, :scores, :turns, :matches
 
 		def initialize(args = {})
-			@code   = args.fetch(:code, default_code)
+			@code   = args.fetch(:code, generate_code)
 			@scores = [0, 0]
 			@turns = []
-			puts "Default Code: #{code.inspect}"
+			@matches = {}
 		end
 
 		def display
@@ -17,23 +16,13 @@ module Mastermind
 			puts ""
 		end
 
-		def show_turns
-			turns.each do |turn|
-				puts "Guess: #{turn[:guess].inspect} Result: #{turn[:result].inspect}"
-			end
-		end
-		
-		def show_scores
-			puts "Score = Player1: #{@scores[0]} Player2: #{@scores[1]}"
-		end
-
 		def compare_codes(guess)
 			result = []
 
-			matches = find_matches(guess)
-			matches.each { result << :black }
+			self.matches = find_matches(guess)
+			matches.each { |slot| result << :black }
 
-			rest = code - matches
+			rest = code - matches.values
 			uniq_similars = rest & guess
 			uniq_similars.each { result << :white }
 
@@ -43,17 +32,43 @@ module Mastermind
 			(guess == code) ? true : false
 		end
 
+		def guess_code
+			if matches.empty?
+				generate_code
+			else
+				4.times.reduce([]) do |guess, index|
+					if matches.keys.include? index
+						guess[index] = matches[index]
+					else
+						guess[index] = rand(6) + 1
+					end
+					guess
+				end
+			end
+		end
+
 		private
 
+		def show_turns
+			turns.each.with_index do |turn, index|
+				print "#{index + 1}.".ljust(4)
+				puts "Guess: #{turn[:guess].inspect} "\
+						 "Result: #{turn[:result].inspect}"
+			end
+		end
+		
+		def show_scores
+			puts "Score = Player1: #{@scores[0]} Player2: #{@scores[1]}"
+		end
 
 		def find_matches(guess)
- 			code.each.with_index.reduce([]) do |matches, (slot, pos)|
-				matches << slot if slot == guess[pos]
+ 			code.each.with_index.reduce({}) do |matches, (slot, pos)|
+				matches[pos] = slot if slot == guess[pos]
 				matches
 			end
 		end
 
-		def default_code
+		def generate_code
 			4.times.reduce([]) do |code|
 				rand_num = rand(6) + 1
 				code << rand_num
--- lib/mastermind/game.rb
+++ lib/mastermind/game.rb
@@ -1,39 +1,62 @@
 module Mastermind
 	class Game
-		attr_reader :player, :board
+		attr_reader :board
+		attr_accessor :player, :role
 		
 		def initialize(args = {})
-			@player = args.fetch(:player, Player.new(role: :computer))
-			@board  = args.fetch(:board, Board.new(code: [1, 1, 2, 2]))
+			@role = :breaker
+			@board  = args.fetch(:board, Board.new)
 		end
 
 		def play
 			puts "Welcome to Mastermind!"
 			puts ""
 
-			count = 0
 			loop do
-				count +=1
-				guess = get_guess
-				break if board.compare_codes(guess) or count == 12
-				board.display
-			end
+				self.role = get_player_role
 
-			if count == 12
-				puts "Sorry, you ran out of tries"
-			else
-				puts "Won this round."
-				board.show_scores
-			end
+				break if role == :quit
 
+				board.code = get_code if role == :maker
+				play_round(role)
+			end
 		end
 
 		private
 
+		def get_player_role
+			puts "Role: codebreaker(1) or codemaker(2) or quit(q): "
+			role = gets.chomp
+			if role == "1"
+				:breaker
+			elsif role == "2"
+				:maker
+			else
+				:quit
+			end
+		end
+
+		def get_code
+			puts "Secret code (1 to 6, seperated by spaces, 4 slots): "
+			gets.chomp.split(" ").map(&:to_i)
+		end
+
+		def play_round(role)
+			count = 0
+			loop do
+				count +=1
+				guess = (role == :maker) ? board.guess_code : get_guess
+				break if board.compare_codes(guess) or count == 12
+				board.display
+			end
+
+			print_finish_round(count)
+		end
+
 		def get_guess
 			guess = []
 			loop do
-				print "Code to test: "
+				print "Your guess: "
 				guess = gets.chomp.split(" ").map(&:to_i)
 				if correct_range?(guess) and guess.size == 4
 					break
@@ -46,8 +69,19 @@ module Mastermind
 			guess
 		end
 
+		def print_finish_round(count)
+			if count == 12
+				puts "Sorry, you ran out of tries"
+			else
+				puts "Won this round."
+			end
+			board.display
+			board.turns = []
+		end
+
 		def correct_range?(guess)
 			guess.all? {|slot| (1..6).include?(slot) }
 		end
+
 	end
 end
